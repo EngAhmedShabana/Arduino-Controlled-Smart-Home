7126c75aa5011dbdc7a8fc5a9568087b21b7baf4
EngAhmedShabana <138723900+EngAhmedShabana@users.noreply.github.com>
2023-08-26 13:51:21 +0300

    Update smart_home1.ino

--- smart_home1.ino
+++ smart_home1.ino
@@ -1,10 +1,10 @@
 byte led[8]={2,3,4,5,6,7,8,9};
 byte light[5]={
-  B11001110, 
-  B10110110,
-  B10000110,
-  B01001110,
-  B00101110};
+  B01110011, // s
+  B01101101, // m
+  B01100001, // a
+  B01110010, // r
+  B01110100}; // t
 char x;
 void setup() {
   for(byte i=0;i<8;i++){
@@ -20,7 +20,7 @@ void loop() {
   if (Serial.available()>0){
     x=Serial.read();
     for(byte i=0;i<8;i++){
-     digitalWrite(led[i],x&B00000001<<i);
+     digitalWrite(led[i],x&1<<i);
     }delay(500);
   }
 }
